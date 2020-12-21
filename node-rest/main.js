a8d62caa7e39320a1147de7cdcb55e18eda5800b
dheerajopensorce <dheeraj.opensource@gmail.com>
2020-12-21 23:11:18 +0530

    added cors

--- node-rest/main.js
+++ node-rest/main.js
@@ -1,7 +1,9 @@
 const { urlencoded } = require('body-parser');
 const express = require('express');
+const cors =  require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended :false}));
 
--- node-rest/routes/api/account.js
+++ node-rest/routes/api/account.js
@@ -3,11 +3,18 @@ const send = require('send');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const uuid = require('uuid');
+const cors = require('cors');
 
 let users = require('../../fake-data/acc.json');
 
+//cors config
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
 //1- post login return json token
-router.post("/login",(req,res)=>{
+router.post("/login",cors(corsOptions),(req,res)=>{
    
     const cred = {
         username: req.body.Username,
